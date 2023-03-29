4f07157d7a28295589b21471a3dcbdf1d8d194cf
tadescalzo <ta.descalzo@gmail.com>
2023-03-29 20:19:24 -0300

    configuracion de rtk todos

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,12 +1,44 @@
+import { useState } from "react"
+import { useGetTodoQuery, useGetTodosQuery } from "../store/api/todosApi"
 
 export const TodoApp = () => {
+
+  const [count, setCount] = useState(1)
+
+
+/* const {data: todos, isLoading} = useGetTodosQuery() */
+const {data: todo, isLoading} = useGetTodoQuery(count)
+
+console.log(todo)
+
+const nextTodo = (count) =>{
+  count === 200 ? setCount(1) : setCount(count+1)
+}
+
+const prevTodo = (count) =>{
+  count === 1 ? setCount(200) : setCount(count-1)
+}
+
   return (
     <>
     <h1>Todo - RTK Queryt</h1>
     <hr />
-    <h4>isLoading...</h4>
-    <pre>...</pre>
-    <button>
+    <h4>isLoading: {isLoading ? 'True':'False'} </h4>
+    <pre> {JSON.stringify(todo)} </pre>
+
+    {/* <ol>
+      { !isLoading && todos.map(({id, title, completed})=> <li key={id} > <strong>{completed ? 'DONE':'PENDING'}</strong> {title} </li> )}
+    </ol> */}
+
+    <button
+    onClick={()=>{prevTodo(count)}}
+    >
+        Prev todo
+    </button>
+
+    <button
+    onClick={()=>{nextTodo(count)}}
+    >
         Next todo
     </button>
     </>
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { todosApi } from './api/todosApi'
 import {counterSlice} from './slices/counter/counterSlice'
 import { pokemonSlice } from './slices/pokemon'
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice.reducer,
-        pokemons: pokemonSlice.reducer
+        pokemons: pokemonSlice.reducer,
+
+        [todosApi.reducerPath]: todosApi.reducer
       },
+
+      middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(todosApi.middleware)
 })
